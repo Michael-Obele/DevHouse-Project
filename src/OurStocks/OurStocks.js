c04a2f725c11c1bd2153144c4fce605dc2029f53
Michael Obubelebra Amachree <70345027+Michael-Obele@users.noreply.github.com>
2022-07-18 01:38:25 +0100

    Added music tag 1

--- src/OurStocks/OurStocks.js
+++ src/OurStocks/OurStocks.js
@@ -2,6 +2,10 @@ import { StockNav } from './StockNav';
 import React from 'react';
 import { useInView } from 'react-intersection-observer';
 import { OurStockText } from './OurStockText';
+import Fela from '../Images/Fela.png';
+import { BsFillCartCheckFill } from 'react-icons/bs';
+import { MdOutlineKeyboardArrowRight } from 'react-icons/md';
+import style from './index.module.css';
 
 export function OurStocks({}) {
   const { ref, inView } = useInView({
@@ -13,12 +17,48 @@ export function OurStocks({}) {
       <OurStockText />
       {/* Typed Character */}
       <StockNav />
-      <div className='pb-[10rem]'></div>
-      <div className='flex flex-row items-center justify-center'>
-        <span className='border-b-2 mx-2 px-[40vw] border-green-700'> {}</span>
-        <p className='bg-transparent py-auto text-gray-600 font-medium text-[1rem]'>
-          Show More
-        </p>
+      <div className=' pb-[10rem] flex flex-row justify-center'>
+        <div className='py-[2rem] px-[4rem] bg-gradient-to-b from-[#1F3180] to-[#1f318000] container rounded-[32px] border-2 border-b-0 border-green-700 h-fit w-fit'>
+          <div className='container flex justify-center items-center mx-auto'>
+            <img src={Fela} alt='Fela Kuti' />
+          </div>
+          <p className='text-center mt-5 text-white text-[1.25em]'>Fela Kuti</p>
+          <p className='text-center text-gray-300 text-[0.9em]'>
+            Sorrow, Tears and Blood
+          </p>
+          <div className='container flex mx-auto py-4'>
+            <input
+              type='range'
+              min='1'
+              max='100'
+              value='71'
+              className={style.slider}
+              id='myRange'></input>
+          </div>
+          <div className='container flex justify-center items-center mx-auto'>
+            <p className='text-white text-center font-[400] text-[1rem] mx-2'>
+              $2,453
+            </p>
+            <span className='text-white mx-2 bg-green-700 rounded px-2 text-center font-[400] text-[1rem]'>
+              71%
+            </span>
+          </div>
+          <span className='flex justify-center'>
+            <button className='bg-white m-2 px-6 py-2 flex flex-row items-center text-black rounded-md'>
+              <BsFillCartCheckFill className='mr-2 mb-1' />
+              Buy Shares
+            </button>
+          </span>
+        </div>
+      </div>
+      <div className='container flex flex-row items-center justify-center'>
+        <span className='border-b-2 mx-2 px-[38vw] border-green-700'> {}</span>
+        <span className='flex flex-row items-center text-white'>
+          <p className='bg-transparent py-auto font-medium text-[1rem]'>
+            Show More
+          </p>
+          <MdOutlineKeyboardArrowRight />
+        </span>
       </div>
     </section>
   );
--- src/OurStocks/StockNav.js
+++ src/OurStocks/StockNav.js
@@ -22,14 +22,14 @@ export function StockNav({}) {
   return (
     <nav
       ref={ref}
-      className='bg-transparent border-gray-400 px-auto sm:px-4 py-2.5 dark:bg-gray-800 w-screen'>
-      <div className='container flex flex-wrap items-center  justify-center m-auto'>
+      className='bg-transparent border-gray-400 py-2.5 dark:bg-gray-800 container'>
+      <div className='container justify-center flex flex-wrap items-center  mx-auto'>
         <div>
           <ul
             className={
               inView
-                ? `${fade.fadeIn} flex flex-col text-[1rem] font-bold mt-4 md:flex-row md:space-x-8 md:mt-0`
-                : 'flex flex-col text-[1rem] font-bold mt-4 md:flex-row md:space-x-8 md:mt-0 '
+                ? `${fade.fadeIn} flex flex-col px-[6rem] text-[1rem] font-bold mt-4 md:flex-row md:space-x-8 md:mt-0`
+                : 'flex flex-col px-[6rem] text-[1rem] font-bold mt-4 md:flex-row md:space-x-8 md:mt-0 '
             }>
             <li>
               <a
@@ -44,7 +44,7 @@ export function StockNav({}) {
                 <a
                   key={index + '_' + item}
                   href='#'
-                  className='block text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-green-400 md:dark:hover:text-green dark:hover:bg-green-700 dark:hover:text-green md:dark:hover:bg-transparent dark:border-green-700'>
+                  className='block mx-[0.7rem]  text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-green-400 md:dark:hover:text-green dark:hover:bg-green-700 dark:hover:text-green md:dark:hover:bg-transparent dark:border-green-700'>
                   {item}
                 </a>
               </li>
