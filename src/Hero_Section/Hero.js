26fef4130ce7b334ddb9ae41c85cb8bd1a97181b
Michael Obubelebra Amachree <70345027+Michael-Obele@users.noreply.github.com>
2022-07-24 21:42:00 +0100

    More from comments

--- src/Hero_Section/Hero.js
+++ src/Hero_Section/Hero.js
@@ -25,18 +25,18 @@ export function Hero() {
         <h1 className='text-[40px] md:text-[64px] font-[700] text-[#2941ab]'>
           Listening isn't
         </h1>
-        <h1 className='text-[40px] md:text-[64px] font-[700] text-[#2941ab] lg:m-4 lg:ml-20'>
+        <h1 className='text-[40px] md:text-[64px] font-[700] text-[#2941ab] lg:m-4 lg:ml-[7.3rem]'>
           everything now
         </h1>
-        <div className='bg-[#1ed760]  rounded-l-[4px] rounded-r-[4px] border-x-[2px] border-l-[#0e173c] border-r-[#0e173c] lg:border-r-0 border-y-0 box-border lg:pt-0 h-24 w-1/2 px-2.5  border-4'>
-          <p className='md:text-justify font-[500] lg:font-[400] text-center text-[12px] md:text-[16px] h-fit w-fit leading-4 lg:leading-5'>
+        <div className='bg-[#1ed760]  rounded-l-[4px] rounded-r-[4px] border-x-[2px] border-l-[#0e173c] border-r-[#0e173c] lg:border-r-0 border-y-0 box-border h-[60px]  w-[491px] px-2.5  border-4 items-center flex flex-col'>
+          <p className='md:text-justify font-[500] lg:font-[400] text-center text-[12px] md:text-[16px] pl-[16px] leading-4 lg:leading-5 h-[44px] w-[491px]  items-center'>
             There is more, you can now buy shares of songs from spotify using
             our smarts contracts and that's all you will ever need to begin
           </p>
         </div>
 
         <Image
-          className={`${fade.fadeIn} w-[135.84px] h-[256px] lg:w-[364px] lg:h-[686px] lg:absolute lg:top-0 lg:right-10 z-[4]`}
+          className={`${fade.fadeIn} w-[135.84px] h-[256px] lg:w-[364px] lg:h-[686px] lg:absolute lg:top-0 lg:left-[100%] z-[4]`}
         />
         {isLoading ? (
           <button className='bg-[#000812] font-bold rounded-[8px] border-transparent px-[2rem] py-[1rem] mt-[5rem]  xl:m-[7rem] lg:mt-[2.9rem] box-border h-fit w-fit border-4 cursor-pointer text-white z-[2] transition-all hover:scale-110 buttonLoader'>
