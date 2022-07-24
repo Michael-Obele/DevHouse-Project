f3b90010c381f6cbdf776e689499a1fed18e856e
Michael Obubelebra Amachree <70345027+Michael-Obele@users.noreply.github.com>
2022-07-24 20:29:51 +0100

    Added show more to tablet

--- src/OurTeam/OurTeam.js
+++ src/OurTeam/OurTeam.js
@@ -8,7 +8,8 @@ function OurTeam() {
     <section id='Team' className='pb-[128px]'>
       <OurTeamText />
       <TeamCards />
-      <div className='container mt-[64px] flex flex-row items-center justify-center md:hidden'>
+      {/* Show More Line */}
+      <div className='container mt-[64px] flex flex-row items-center justify-center lg:hidden'>
         <span className='border-b-[.0625rem] ml-[4rem] mr-[2rem] pl-[65vw] lg:pl-[78vw] border-[#1ED760]'></span>
         <span className='flex flex-row items-center text-white cursor-pointer'>
           <p className='bg-transparent py-auto font-medium text-[1rem]'>
