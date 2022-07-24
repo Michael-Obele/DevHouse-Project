f7e19c625a69842ca42f3354be96d034e5c84cb7
Michael Obubelebra Amachree <70345027+Michael-Obele@users.noreply.github.com>
2022-07-24 21:52:42 +0100

    Additions

--- src/Hero_Section/Hero.js
+++ src/Hero_Section/Hero.js
@@ -28,8 +28,8 @@ export function Hero() {
         <h1 className='text-[40px] md:text-[64px] font-[700] text-[#2941ab] lg:m-4 lg:ml-[7.3rem]'>
           everything now
         </h1>
-        <div className='bg-[#1ed760]  rounded-l-[4px] rounded-r-[4px] border-x-[2px] border-l-[#0e173c] border-r-[#0e173c] lg:border-r-0 border-y-0 box-border h-[60px]  w-[491px] px-2.5  border-4 items-center flex flex-col'>
-          <p className='md:text-justify font-[500] lg:font-[400] text-center text-[12px] md:text-[16px] pl-[16px] leading-4 lg:leading-5 h-[44px] w-[491px]  items-center'>
+        <div className='bg-[#1ed760] rounded-l-[4px] rounded-r-[4px] border-x-[2px] border-l-[#0e173c] border-r-[#0e173c] lg:border-r-0 border-y-0 box-border h-[60px] w-[491px] border-4 flex flex-col'>
+          <p className='md:text-justify font-[500] lg:font-[400] text-center text-[12px] md:text-[16px] pl-[16px] lg:leading-5 h-[44px] w-[491px]'>
             There is more, you can now buy shares of songs from spotify using
             our smarts contracts and that's all you will ever need to begin
           </p>
